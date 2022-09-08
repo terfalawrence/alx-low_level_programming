06bed7f055b4e843bd5d8a6df7a189cfa9bed499
terfalawrence <terry14uandlawrence@gmail.com>
2022-09-08 16:33:19 -0700

    c 5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 
 	printf("Size of a char: %1d byte(s)\n", sizeof(c));
 	printf("Size of an int: %1d byte(s)\n", sizeof(i));
-	printf("size of a long int: %1d byte(s)\n", sizeof(li))
+	printf("Size of a long int: %1d byte(s)\n", sizeof(li))
 	printf("Size of a long lomg int: %1d byte(s)\n", sizeof(lli));
 	printf("Size of a float: %1d byte(s)\n", sizeof(f));
 	return (0);
