bc207701793f496fae50abf0a9de0742722f78fa
terfalawrence <terry14uandlawrence@gmail.com>
2022-09-09 11:25:51 -0700

    var 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - entry block
+  * @void: no argument
+  * Return: 0
+**/
+int main(void)
+{
+	char c;
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; C <= 'Z'; C++)
+		putchar(C);
+	putchar('\n');
+	return (0);
+}	
